8518b3a066ae34bf95babb127e449eaec3de0084
Joel Ye <joelye9@gmail.com>
2018-02-05 21:41:16 -0500

    trying to set up initial build

---
+++ server/graphql.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function graphQlRoutes(app) {
+}
+exports.default = graphQlRoutes;
+//# sourceMappingURL=graphql.js.map
