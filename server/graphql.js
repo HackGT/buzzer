1e8286239d6c474049088ba95a7afafe2894b3ed
Joel Ye <joelye9@gmail.com>
2018-02-07 10:12:12 -0500

    Initial server config

--- server/graphql.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function graphQlRoutes(app) {
-}
-exports.default = graphQlRoutes;
-//# sourceMappingURL=graphql.js.map
