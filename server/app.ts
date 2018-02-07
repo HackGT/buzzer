0bce90a9b3d910b615b5c4e7ff1590558901978a
Joel Ye <joelye9@gmail.com>
2018-02-07 10:12:12 -0500

    Initial server config

--- server/app.ts
+++ server/app.ts
@@ -1,64 +1,133 @@
-const express = require('express');
-const compression = require('compression');
-const bodyParser = require('body-parser');
-const { graphqlExpress, graphiqlExpress } = require('graphql-server-express');
-const { makeExecutableSchema } = require('graphql-tools');
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const compression = require('compression')
+const bodyParser = require('body-parser')
 
-// const { PORT } = require('./common');
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import { makeExecutableSchema } from 'graphql-tools';
 
-// import {
-//  PORT
-// } from "./common";
+import { PORT } from './common';
 
-// export const app = express();
-app = express();
+const typeDefs = fs.readFileSync(path.resolve(__dirname, "../api.graphql"), "utf8");
+
+const app = express();
 
 app.use(compression());
 process.on("unhandledRejection", err => {
   throw err;
 });
 
-// Some fake data
-const books = [
+// API suggestions: Use tags and media. Media don't have options (except slack with specified channels). Tags is a PM (through... what platform?) to everyone tagged. - ie Tags is a keyword to send individual messages?
+
+// Temp data
+
+type User = {
+  name: string,
+  phone: string,
+  tags: string[]
+}
+
+type Tag = {
+  name: string,
+  users: string[]
+}
+
+/*
+enum Medium {
+  SLACK,
+  MOBILE
+}
+*/
+
+enum Status {
+  SUCCESS,
+  FAILURE
+}
+
+const users: User[] = [
   {
-    title: "Harry Potter and the Sorcerer's stone",
-    author: 'J.K. Rowling',
+    name: 'Joel Ye',
+    phone: '7187558248',
+    tags: ['ALL', 'Student'],
   },
   {
-    title: 'Jurassic Park',
-    author: 'Michael Crichton',
+    name: 'John Doe',
+    phone: '1234567890',
+    tags: ['ALL', 'Mentor']
   },
 ];
 
-// The GraphQL schema in string form
-const typeDefs = `
-  type Query { books: [Book] }
-  type Book { title: String, author: String }
-`;
+const tags: { [name: string] : Tag }  = {
+  'ALL': {
+    name: 'ALL',
+    users: ['Joel Ye', 'John Doe']
+  },
+  'Student': {
+    name: 'Student',
+    users: [],
+  },
+  'Mentor': {
+    name: 'Mentor',
+    users: []
+  }
+}
 
-// The resolvers
-const resolvers = {
-  Query: { books: () => books },
+// Do I need to use a promise below?
+type APICallback = (message: string) => string;
+
+// Test api
+const slackAPI: APICallback = (message) => {
+  console.log("Slacking")
+  console.log(`Relaying ${message}`)
+  console.log(Status.SUCCESS)
+  return 'SUCCESS'; // Status.SUCCESS is just a number...
+}
+
+
+// Enum keyed dictionaries don't appear to be supported yet
+// const mediaAPI: { [medium: Medium]: APICallback } = {
+const mediaAPI: { [medium: string]: APICallback } = {
+  "SLACK": slackAPI,
+  "MOBILE": ( message: string ) => 'ERROR'// Status.FAILURE
 };
 
-// Put together a schema
+
+const resolvers = {
+  Query: {
+    // sendMessage: (message: string, media: Medium[]) => media.map(
+    // sendMessage: (prev: any, args: any) => { console.log('resolving sendMessage');return {'message' : args.message} },
+    
+    sendMessage: (prev: any, args: any) => args.media.map(
+	(medium : string) => mediaAPI[medium.toUpperCase()](args.message)
+    ),
+    user: (name: string) => users.filter(e => e.name === name)[0],
+    users: () => users,
+    tag: (name: string) => tags[name],
+    tags: () => tags.values
+  },
+  User: {
+    tags: (prev: any) => prev.tags.map((tagName:string) => tags[tagName])
+  }
+  /* ,
+  sendMessage: {
+    Slack: (prev: any, args: any) => {console.log('helloadfkjl'); return mediaAPI['SLACK'](prev.message)} ,
+    Mobile: (prev:any, args: any) => mediaAPI['MOBILE'](prev.message)
+  }
+  */
+}
+
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
 });
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
-
-// graphQlRoutes(app);
-app.listen(3000, () => {
-  console.log(`Buzzer system started on port 3000`);
-});
-/* 
+ 
 app.listen(PORT, () => {
   console.log(`Buzzer system started on port ${PORT}`);
 });
-*/
-module.exports = app;
+
+export default app;
