6203d2e94a629e90b31e00d5a82e74faacec8ca3
Joel Ye <joelye9@gmail.com>
2018-02-10 18:21:22 -0500

    reoutfitted graphql, async is too hard for me

--- server/app.ts
+++ server/app.ts
@@ -1,16 +1,16 @@
-const fs = require('fs')
+const fs = require('fs') 
 const path = require('path')
 const express = require('express')
 const compression = require('compression')
 const bodyParser = require('body-parser')
 
-import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
-import { makeExecutableSchema } from 'graphql-tools';
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express'
+import { makeExecutableSchema } from 'graphql-tools'
 
-import { PORT } from './common';
+import { PORT } from './common'
 
 const typeDefs = fs.readFileSync(path.resolve(__dirname, "../api.graphql"), "utf8");
-
+// TODO: Scan for plugins and import both api and typeDefs. API should take message and config.
 const app = express();
 
 app.use(compression());
@@ -20,114 +20,81 @@ process.on("unhandledRejection", err => {
 
 // API suggestions: Use tags and media. Media don't have options (except slack with specified channels). Tags is a PM (through... what platform?) to everyone tagged. - ie Tags is a keyword to send individual messages?
 
-// Temp data
-
-type User = {
-  name: string,
-  phone: string,
-  tags: string[]
-}
-
-type Tag = {
-  name: string,
-  users: string[]
-}
-
 /*
-enum Medium {
-  SLACK,
-  MOBILE
+enum Status {
+  SUCCESS,
+  FAILURE,
+  NULL
 }
 */
 
-enum Status {
-  SUCCESS,
-  FAILURE
-}
-
-const users: User[] = [
-  {
-    name: 'Joel Ye',
-    phone: '7187558248',
-    tags: ['ALL', 'Student'],
-  },
-  {
-    name: 'John Doe',
-    phone: '1234567890',
-    tags: ['ALL', 'Mentor']
-  },
-];
-
-const tags: { [name: string] : Tag }  = {
-  'ALL': {
-    name: 'ALL',
-    users: ['Joel Ye', 'John Doe']
-  },
-  'Student': {
-    name: 'Student',
-    users: [],
-  },
-  'Mentor': {
-    name: 'Mentor',
-    users: []
-  }
-}
-
 // Do I need to use a promise below?
-type APICallback = (message: string) => string;
+type APICallback = (message: string, config: any) => string;
+type IStatusReturn = { [medium: string]: string }
 
 // Test api
-const slackAPI: APICallback = (message) => {
+const slackAPI: APICallback = ( message, config ) => {
   console.log("Slacking")
   console.log(`Relaying ${message}`)
-  console.log(Status.SUCCESS)
+  console.log(config)
   return 'SUCCESS'; // Status.SUCCESS is just a number...
 }
 
+const liveSiteAPI: APICallback = ( message, config ) => {
+  console.log("Live Site")
+  console.log(`Relaying ${message}`)
+  console.log(config)
+  return `SUCCESS`;
+}
 
-// Enum keyed dictionaries don't appear to be supported yet
-// const mediaAPI: { [medium: Medium]: APICallback } = {
+
 const mediaAPI: { [medium: string]: APICallback } = {
+  "LIVESITE": liveSiteAPI,
   "SLACK": slackAPI,
-  "MOBILE": ( message: string ) => 'ERROR'// Status.FAILURE
-};
-
+  "EMAIL": ( message: string ) => 'ERROR'// Status.FAILURE
+}
 
 const resolvers = {
   Query: {
-    // sendMessage: (message: string, media: Medium[]) => media.map(
-    // sendMessage: (prev: any, args: any) => { console.log('resolving sendMessage');return {'message' : args.message} },
-    
-    sendMessage: (prev: any, args: any) => args.media.map(
-	(medium : string) => mediaAPI[medium.toUpperCase()](args.message)
-    ),
-    user: (name: string) => users.filter(e => e.name === name)[0],
-    users: () => users,
-    tag: (name: string) => tags[name],
-    tags: () => tags.values
-  },
-  User: {
-    tags: (prev: any) => prev.tags.map((tagName:string) => tags[tagName])
+    sendMessage: (prev: any, args: any) => {
+      let statusRet: IStatusReturn = {}
+      const src = Object.keys(args.plugins)
+      src.forEach( plugin => {
+	const pluginFunc = args.plugins[plugin]
+	statusRet[plugin] =
+	  mediaAPI[plugin.toUpperCase()](args.message, pluginFunc)
+      })
+      return statusRet
+      /*
+      async runAPI = () => await Promise.all(src.map( plugin => 
+	mediaAPI[plugin.toUpperCase()](args.message,
+					args.plugins[plugin])
+      ))
+      runAPI().then(
+	res => {
+	  console.log(res)
+	  let statusRet: IStatusReturn = {}
+	  src.forEach( (srcName, index) => {
+	    statusRet[srcName] = res[index]
+	  })
+	  return statusRet
+	})
+      */
+    }
   }
-  /* ,
-  sendMessage: {
-    Slack: (prev: any, args: any) => {console.log('helloadfkjl'); return mediaAPI['SLACK'](prev.message)} ,
-    Mobile: (prev:any, args: any) => mediaAPI['MOBILE'](prev.message)
-  }
-  */
 }
 
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
-});
+})
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
-app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema }))
+app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }))
 
  
 app.listen(PORT, () => {
-  console.log(`Buzzer system started on port ${PORT}`);
-});
+  console.log(`Buzzer system started on port ${PORT}`)
+})
 
-export default app;
+export default app
