2712d32033a0c523c2f224cf7252c715446119e4
Joel Ye <joelye9@gmail.com>
2018-02-18 16:37:31 -0500

    linted

--- server/plugins/api/Slack.ts
+++ server/plugins/api/Slack.ts
@@ -1,33 +1,31 @@
-import { APIReturn } from "./APIReturn"
-import { GenericNotifier } from "./GenericNotifier"
-import { GenericConfig } from "./GenericConfig"
+import { APIReturn } from "./APIReturn";
+import { GenericNotifier } from "./GenericNotifier";
+import { GenericConfig } from "./GenericConfig";
 
 interface Config extends GenericConfig {
-  groups?: string[],
-  channel?: string,
-    channels?: string[],
-    message: string
+  groups?: string[];
+  channel?: string;
+  channels?: string[];
+  message: string;
 }
 
 export default class Slack implements GenericNotifier {
-    sendMessage = (config : Config) : APIReturn => {
-        // plugin code here
+    public sendMessage = (config: Config): APIReturn => {
         console.log(config.message);
         if (config.groups !== undefined) {
             console.log(config.groups);
         }
-        // do something with channels here
         if (config.channels !== undefined) {
             console.log(config.channels);
         }
         return {
           error: 0,
-	  debugInfo: {
-	    "Channels" : config.channels,
-	    "Channel" : config.channel
-	  }
-        }
+					debugInfo: {
+						"Channels" : config.channels,
+						"Channel" : config.channel
+					}
+        };
     }
 
-    TAG = "SLACK"
+    public TAG = "SLACK";
 }
