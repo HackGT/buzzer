a9902cbbf8c129a656b49ae382fc92eb24ffe6c3
Stephanie <almeidasmail@gmail.com>
2018-02-20 15:43:02 -0500

    Fixed most errors, but have new error with fetch call

--- slack.ts
+++ slack.ts
@@ -1,17 +1,13 @@
-/** Erors:
-    - lines 5+6: says cannot find modules for http and querystring
-    - line 9: says cannot find name process
-    - line 29: don't know type of request
-    - line 37: says can't find Buffer*/
+/** Error20:
+    - line 21: don't know how to declare call signature in tsc*/
 
-import * as Slack from "slack";
-import * as http from "http";
 import * as querystring from "querystring";
+import * as fetch from "node-fetch";
 
 const tkn = process.env.SLACK_TOKEN;
 
-function slackChat(slackDomain : string, slackChannel : string, slackMessage : string) {
-    var message = {
+export async function slackChat(slackDomain : string, slackChannel : string, slackMessage : string) {
+    const message = {
         token: tkn,
         channel: slackChannel,
         as_user: false,
@@ -19,24 +15,10 @@ function slackChat(slackDomain : string, slackChannel : string, slackMessage : s
         text: slackMessage
     };
 
-    var qs = querystring.stringify(message);
+    const qs = querystring.stringify(message);
 
-    var options = {
-        "method": "GET",
-        "hostname": slackDomain + "slack.com",
-        "path": "api/chat.postMessage?" + qs
-    };
-
-    var req = http.request(options, function(res) {
-        var chunks : String[] = [];
-
-        res.on("data", function(chunk : String) {
-            chunks.push(chunk);
-        });
-
-        res.on("end", function() {
-            var body = Buffer.concat(chunks);
-            console.log(body.toString());
-        });
-    });
+    const res = await
+        fetch(`https://${slackDomain}.slack.com/api/chat.postMessage?${qs}`);
+    const json = await res.json();
+    console.log(json);
 }
