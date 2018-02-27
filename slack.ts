94da0e3eaeaedb05f8f08c090c511703094858fd
Stephanie <almeidasmail@gmail.com>
2018-02-27 14:46:23 -0500

    Edited file to include interface methods

--- slack.ts
+++ slack.ts
@@ -2,23 +2,53 @@
     - line 21: don't know how to declare call signature in tsc*/
 
 import * as querystring from "querystring";
-import * as fetch from "node-fetch";
+import fetch from "node-fetch";
 
 const tkn = process.env.SLACK_TOKEN;
+const domain = process.env.DOMAIN;
 
-export async function slackChat(slackDomain : string, slackChannel : string, slackMessage : string) {
-    const message = {
+async function sendMessage(message : string, config : string) : Promise<void> {
+    const msg = {
         token: tkn,
-        channel: slackChannel,
+        channel: config,
         as_user: false,
         username: "HackGT",
-        text: slackMessage
+        text: message
     };
 
-    const qs = querystring.stringify(message);
+    const qs = querystring.stringify(msg);
 
     const res = await
-        fetch(`https://${slackDomain}.slack.com/api/chat.postMessage?${qs}`);
+        fetch(`https://${domain}.slack.com/api/chat.postMessage?${qs}`);
     const json = await res.json();
     console.log(json);
+}
+
+async function check(config : any): Promise<string> {
+    const res = await
+        fetch(`https://${domain}.slack.com/api/channels.list`);
+    const channels = await res.json();
+
+    const res2 = await
+        fetch(`https://${domain}.slack.com/api/groups.list`);
+    const groups = await res2.json();
+
+    var resultsChannels : string[] = [];
+    var resultsGroups : string[] = [];
+    var searchField = 'name';
+    var searchValue = config;
+
+    for (var i = 0; i < channels.length; i++) {
+        if (channels[i][searchField] == searchValue) {
+            resultsChannels.push(channels[i]);
+            return config;
+        }
+    }
+    for (var i = 0; i < groups.length; i++) {
+        if (channels[i][searchField] == searchValue) {
+            resultsGroups.push(groups[i]);
+            return config;
+        }
+    }
+    return "Channel does not exist!";
 }
