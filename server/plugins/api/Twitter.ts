cb16e3a34708b8be5305c0588ec59eb4760b653e
Joel Ye <joelye9@gmail.com>
2018-02-27 16:44:07 -0500

    Moved env vars to class vars

--- server/plugins/api/Twitter.ts
+++ server/plugins/api/Twitter.ts
@@ -5,33 +5,41 @@ import * as TwitterAPI from "twit";
 
 export default class Twitter implements GenericNotifier<{}> {
 
-	public async setup(): Promise<void> {
+	private consumerKey: string;
+	private consumerSecret: string;
+	private accessToken: string;
+	private accessTokenSecret: string;
+
+	constructor() {
 		const consumerKey = process.env.TWITTER_CONSUMER_KEY;
 		const consumerSecret = process.env.TWITTER_CONSUMER_SECRET;
 		const accessToken = process.env.TWITTER_HACKGT_ACCESS_TOKEN;
 		const accessTokenSecret = process.env.TWITTER_HACKGT_ACCESS_TOKEN_SECRET;
-		let envFlag: boolean = false;
+
 		if (!consumerKey) {
-			console.log("Missing Twitter consumer_key env var");
-			envFlag = true;
+			console.error("Missing Twitter consumer_key env var");
 		}
 		if (!consumerSecret) {
-			console.log("Missing Twitter consumer_secret env var");
-			envFlag = true;
+			console.error("Missing Twitter consumer_secret env var");
 		}
 		if (!accessToken) {
-			console.log("Missing Twitter access_token env var");
-			envFlag = true;
+			console.error("Missing Twitter access_token env var");
 		}
 		if (!accessTokenSecret) {
-			console.log("Missing Twitter access_token_secret env var");
-			envFlag = true;
+			console.error("Missing Twitter access_token_secret env var");
 		}
 
-		if ( envFlag ) {
+		if (!consumerKey || !consumerSecret || !accessToken || !accessTokenSecret) {
 			throw new Error("Twitter env vars missing, aborting...");
 		}
 
+		this.consumerKey = consumerKey;
+		this.consumerSecret = consumerSecret;
+		this.accessToken = accessToken;
+		this.accessTokenSecret = accessTokenSecret;
+	}
+
+	public async setup(): Promise<void> {
 		return; // No setup currently
 	}
 
@@ -41,31 +49,31 @@ export default class Twitter implements GenericNotifier<{}> {
 
 	public async sendMessage(message: string, config: {}): Promise<[APIReturn]> {
 		const client = new TwitterAPI({
-			consumer_key: (process.env.TWITTER_CONSUMER_KEY as string),
-			consumer_secret: (process.env.TWITTER_CONSUMER_SECRET as string),
-			access_token: (process.env.TWITTER_HACKGT_ACCESS_TOKEN as string),
-			access_token_secret: (process.env.TWITTER_HACKGT_ACCESS_TOKEN_SECRET as string)
+			consumer_key: this.consumerKey,
+			consumer_secret: this.consumerSecret,
+			access_token: this.accessToken,
+			access_token_secret: this.accessTokenSecret
 		});
 		const params = { status: message };
-		const res = await new Promise((resolve, reject) => {
-			client.post('statuses/update', params, (error: any, data: any, response: any) => {
+		const res: Promise<APIReturn> = new Promise((resolve, reject) => {
+			client.post('statuses/update', params, (error, data: any, response) => {
 				if (!error) {
-					const url = `https:\/\/twitter.com\/${data.user.screen_name}\/status\/${data.id_str}`;
+					const url = `https://twitter.com/${data.user.screen_name}/status/${data.id_str}`;
 					resolve({
 						error: false,
-						key: "Twitter",
+						key: "twitter",
 						message: `Successful tweet, view at ${url}`
 					});
 				} else {
 					resolve({
 						error: true,
-						key: "Twitter",
+						key: "twitter",
 						message: `${error.message}`
 					});
 				}
 			});
 		});
-		return [(res as APIReturn)];
+		return [await res];
 	}
 
 }
