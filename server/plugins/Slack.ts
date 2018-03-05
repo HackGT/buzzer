05d2b8e502dbc55d387712b180d2854a71245d28
Stephanie <almeidasmail@gmail.com>
2018-03-04 21:21:55 -0500

    fixed errors

--- server/plugins/Slack.ts
+++ server/plugins/Slack.ts
@@ -1,132 +1,111 @@
 import * as querystring from "querystring";
 import fetch from "node-fetch";
-import {Plugin, GenericNotifier} from "./GenericNotifer";
-import APIReturn from "./APIReturn";
+import {Plugin, GenericNotifier} from "./GenericNotifier";
+import {APIReturn} from "./APIReturn";
 
 interface Config {
 	channels: string[];
 }
 
 class Slack implements GenericNotifier<Config> {
-    private token: string;
-    private domain: string;
+	private token: string;
+	private domain: string;
+	private username: string;
 
-    constructor() {
-        const token = process.env.SLACK_TOKEN;
-        const domain = process.env.SLACK_DOMAIN;
+	constructor() {
+		const token = process.env.SLACK_TOKEN;
+		const domain = process.env.SLACK_DOMAIN;
+		const username = process.env.SLACK_USERNAME;
 
-        if (!token) {
-            console.error("Missing Slack token env var");
-        }
-        if (!domain) {
-            console.error("Missing Slack domain env var");
-        }
+		if (!token) {
+			console.error("Missing Slack token env var");
+		}
+		if (!domain) {
+			console.error("Missing Slack domain env var");
+		}
+		if (!username) {
+			console.error("Missing Slack username env var");
+		}
 
-        if (!token || !domain) {
+		if (!token || !domain || !username) {
 			throw new Error("Slack env vars missing, aborting...");
 		}
 
-        this.token = token;
-        this.domain = domain;
-    }
+		this.token = token;
+		this.domain = domain;
+	}
 
-    public async sendMessage(message : string, config : any) : Promise<APIReturn> {
-        await Promise.all(config.channels.map(chan => (async () => {
-            const msg = {
-                token: this.token,
-                channel: chan,
-                as_user: false,
-                username: "HackGT",
-                text: message
-            };
+	public async sendMessage(message: string, config: Config): Promise<APIReturn[]> {
+		return await Promise.all(config.channels.map(chan => (async () => {
+			const msg = {
+				token: this.token,
+				channel: chan,
+				as_user: false,
+				username: this.username,
+				text: message
+			};
 
-            const qs = querystring.stringify(msg);
+			const qs = querystring.stringify(msg);
 
-            const res = await fetch(`https://${this.domain}.slack.com/api/chat.postMessage?${qs}`);
-            const json = await res.json();
-            let js = JSON.parse(json);
-            for (var a in js) {
-                if (!js.hasOwnProperty(a)) continue;
-                if (a == 'ok') {
-                    if (js[a] == false) {
-                        return [{
-                            error: true,
-                            key: "Slack Master",
-                            message: js[a + 1]
-                        }];
-                    }
-                }
-            }
-            return [{
-                error: false,
-                key: "Slack Master",
-                message: "Message posted at channel: " + chan
-            }];
-        })));
-    }
+			const res = await fetch(`https://${this.domain}.slack.com/api/chat.postMessage?${qs}`);
+			const json = await res.json();
 
-    public async setup(): Promise<void> {
-        return;
-    }
+			return {
+				error: !json.ok,
+				key: chan,
+				message: json.ok? "Message posted at channel: " + chan : json.error
+			};
+		})()));
+	}
 
-    public async check(config : any): Promise<Config> {
-        var params = {
-            token: this.token,
-            exclude_archived: true,
-            exclude_members: true
-        };
+	public async check(configTest: any): Promise<Config> {
 
-        const qs = querystring.stringify(params);
+		const qs = querystring.stringify({
+			token: this.token,
+			exclude_archived: true,
+			exclude_members: true
+		});
 
-        this.instanceOfConfig(config);
-            const res = await fetch(`https://${this.domain}.slack.com/api/channels.list?${qs}`);
-            const channels = await res.json();
+		const config = this.instanceOfConfig(configTest);
 
-            const res2 = await fetch(`https://${this.domain}.slack.com/api/groups.list?${qs}`);
-            const groups = await res2.json();
+		if (config.channels.length === 0) {
+			throw new Error('Must specify at least one slack channel');
+		}
 
-            var isFound = false;
-            for (var i = 0; i < config.channels.length; i++) {
-                var searchValue = config.channels[i];
-                if (!(channels.map(c => c.name).includes(searchValue)) && !(groups.map(c => c.name).includes(searchValue))) {
-                    isFound = false;
-                    throw new Error("One of the channels' names is invalid");
-                } else {
-                    isFound = true;
-                }
-            }
+		const res = await fetch(`https://${this.domain}.slack.com/api/channels.list?${qs}`);
+		const channels: { name: string }[] = await res.json();
 
-            if (isFound) {
-                return (config as Config);
-            } else {
-                throw new Error("One of the channels' names is invalid");
-            }
-    }
+		const res2 = await fetch(`https://${this.domain}.slack.com/api/groups.list?${qs}`);
+		const groups: { name: string }[] = await res2.json();
 
-    instanceOfConfig(object : any) {
-        const channelsCheck: boolean =
+		const everything = channels.map(c => c.name).concat(groups.map(c => c.name));
+
+		const invalid = config.channels.filter(c => !everything.includes(c));
+
+		if (invalid.length !== 0) {
+			throw new Error(`Invalid slack channels / groups: ${invalid}`);
+		}
+		return config;
+	}
+
+	private instanceOfConfig(object: any): Config {
+		const channelsCheck: boolean =
 			Array.isArray(object.channels) &&
 			(object.channels as any[]).every(channel =>
 				typeof channel === "string"
 			);
-		if (channelsCheck) {
-			return (true);
-		} else {
-			throw new Error("Slack config incorrect.");
-        }
-    }
-
+		if (!channelsCheck) {
+			throw new Error("Slack config should have a channels variable which is an array of strings.");
+		}
+		return object as Config;
+	}
 }
 
-function instanceOfConfig(object : any) {
-    return 'channels' in object;
-}
-
-const SlackPlugin: Plugin<{}> = {
+const SlackPlugin: Plugin<Config> = {
 	schema: () => `{
-		channels: [String!]
+		channels: [String!]!
 	}`,
 	init: async () => new Slack()
 };
 
-export default SlackPlugin
+export default SlackPlugin;
