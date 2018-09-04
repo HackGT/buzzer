97dc310bc2b33722338349042579a59ba5c347c0
Joel Ye <joelye9@gmail.com>
2018-09-04 09:32:50 -0400

    change to copilot

--- server/plugins/Twilio.ts
+++ server/plugins/Twilio.ts
@@ -9,52 +9,66 @@ interface Config {
 export class TwilioNotifier implements Notifier<Config> {
 	private sid: string;
 	private token: string;
+	private serviceSid: string;
+
 	// Refactor note: would like to have local client instead of sid, token, ts difficulties
 	constructor() {
 		const sid = process.env.TWILIO_SID;
 		const token = process.env.TWILIO_TOKEN;
-
+		const serviceSid = process.env.TWILIO_SERVICE_SID;
 		if (!sid) {
 			console.error("Missing TWILIO_SID!");
 		}
 		if (!token) {
 			console.error("Missing TWILIO_TOKEN!");
 		}
+		if (!serviceSid) {
+			console.error("Missing TWILIO_SERVICE_SID");
+		}
 
-		if (!sid || !token) {
+		if (!sid || !token || !serviceSid) {
 			throw new Error("Missing twilio env vars. exiting.");
 		}
 
 		this.sid = sid;
 		this.token = token;
+		this.serviceSid = serviceSid;
+		console.log(serviceSid);
+	}
+
+	// Should provide a programmatic way of setting up service and numbers that runs once
+	// For now phone number setup done on Twilio dashboard https://www.twilio.com/console/sms/services
+	public async setupService() {
+		console.log("Twilio programmatic setup not implemented");
 	}
 
 	private async sendOneMessage(message: string, recipient: string): Promise<PluginReturn> {
 		const client = Twilio(this.sid, this.token);
-		return client.messages.create({
-			body: message,
-			to: recipient,
-			from: '+14702602302' // From a valid Twilio number
-		})
-		.then((msg) => {
-			return {
-				error: false,
-				key: recipient,
-				message: `Identifier: ${msg.sid}`
-			};
-		})
-		.catch((err) => {
-			return {
-				error: true,
-				key: recipient,
-				message: `${err}`
-			};
-		});
+		return client
+			.messages
+			.create({
+				body: message,
+				messagingServiceSid: this.serviceSid,
+				to: recipient
+			})
+			.then((msg) => {
+				return {
+					error: false,
+					key: recipient,
+					message: `Identifier: ${msg.sid}`
+				};
+			})
+			.catch((err) => {
+				return {
+					error: true,
+					key: recipient,
+					message: `${err}`
+				};
+			});
 	}
 
 	public async sendMessage(message: string, config: Config): Promise<PluginReturn[]> {
-		// Should use copilot to manage sending of messages - WIP
-		console.log(`sending message ${message} with config ${config.numbers}`);
+		console.log(`Sending message ${message} with config ${config.numbers}`);
 		return await Promise.all(config.numbers.map((phoneNumber) => {
 			return this.sendOneMessage(message, phoneNumber);
 		}));
