6c81ee219e5ad36dfab647be6f8b6f4cc4a8f774
Rahul Rajan <rahulrajan.us@gmail.com>
2019-03-23 23:30:29 -0400

    Implemented message-logging with nedb and graphql

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,7 @@ import * as express from "express";
 import * as compression from "compression";
 import * as bodyParser from "body-parser";
 import * as Datastore from "nedb";
+import * as dotenv from "dotenv";
 
 import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
@@ -9,12 +10,17 @@ import { makeExecutableSchema } from 'graphql-tools';
 import config from './config';
 import { upperCamel } from './common';
 import { mediaAPI } from './plugins';
-import { PluginReturn, Notifier } from './plugins/Plugin';
+import { PluginReturn, Notifier, MetaDataType } from './plugins/Plugin';
 import typeDefs from './typeDefs';
 import { isAdmin } from './middleware';
 
 const app = express();
-const db = new Datastore({ filename: './server/datastore/message_log.db', autoload: true, timestampData: true });
+dotenv.config();
+const db: any = {};
+Object.keys(mediaAPI).forEach(key => {
+	const file = `./server/datastore/${key.toLowerCase()}_log.db`;
+	db[key] = new Datastore({ filename: file, autoload: true, timestampData: true });
+});
 
 app.use(compression());
 process.on("unhandledRejection", err => {
@@ -26,32 +32,46 @@ interface IPluginReturn {
 	errors: PluginReturn[];
 }
 
+interface IMessageReturn {
+	message: string;
+	config: MetaDataType;
+	_id: string;
+	createdAt: string;
+	updatedAt: string;
+}
+
 let plugins: {
 	[name: string]: Notifier<any>;
 } = {};
 
 const resolvers = {
 	Query: {
+		get_messages: async (prev: any, args: any): Promise<IMessageReturn[]> => {
+			let plugin = args.plugin;
+			let returnDocs = await new Promise<IMessageReturn[]>(resolve => {
+				db[upperCamel(plugin)].find({}, (err: any, docs: any) => {
+					resolve(docs);
+				});
+			});
+
+			return returnDocs;
+		},
 		send_message: async (prev: any, args: any): Promise<IPluginReturn[]> => {
 			const message = args.message;
 
-			const checkQueue = Object.keys(args.plugins).map( rawName => {
+			const checkQueue = Object.keys(args.plugins).map(rawName => {
 
 				return (async () => { // Loading checkQueue IIFE
 					// Upper Cameled
 					const name = upperCamel(rawName);
 					const plugin = plugins[name];
 					const verifiedConfig = await plugin.check(args.plugins[rawName]); // Verify
-
 					return async () => { // Sending function
 						try {
 							const pluginReturn = {
 								plugin: name,
 								errors: await plugin.sendMessage(message, verifiedConfig)
 							};
-
-							db.insert(args); // Inserts the args object from GraphQL query into database
-
 							return pluginReturn;
 						}
 						catch (e) {
@@ -69,12 +89,27 @@ const resolvers = {
 			});
 
 			const sendingQueue = await Promise.all(checkQueue);
-			return await Promise.all(sendingQueue.map(f => f())); // Send all!
+			return await Promise.all(sendingQueue.map(f => {
+
+				f().then(result => {
+					const p = result.plugin.split(/(?=[A-Z])/).join('_').toLowerCase();
+					const insertArg = {
+						message: args.message,
+						config: args.plugins[p],
+						_id: args._id,
+						createdAt: args.createdAt,
+						errors: result.errors
+					};
+					db[upperCamel(result.plugin)].insert(insertArg);
+				}).catch(err => console.log(err));
+				return f();
+			})); // Send all!
 		}
 	}
 };
 
-const schema = makeExecutableSchema({ typeDefs,
+const schema = makeExecutableSchema({
+	typeDefs,
 	resolvers
 });
 
@@ -87,8 +122,6 @@ app.use(
 	})
 );
 app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
-// Endpoint for getting messages sent, sorted by most recent
-app.get('/message_log', (req, res) => db.find({}).sort({ createdAt: -1 }).exec((err: any, docs: any) => res.send(docs)));
 
 // Run plugin setup
 async function runSetup() {
--- server/plugins/Plugin.ts
+++ server/plugins/Plugin.ts
@@ -13,3 +13,34 @@ export interface Notifier<T> {
 	sendMessage(message: string, config: T): Promise<PluginReturn[]>;
 	check(config: any): Promise<T>;
 }
+
+export interface LiveSiteConfigType {
+	title: string;
+	icon: string;
+}
+
+export interface SlackConfigType {
+	channels: [string];
+	at_channel: boolean;
+	at_here: boolean;
+}
+
+export interface TwilioConfigType {
+	numbers: [string];
+	groups: [string];
+}
+
+export interface TwitterConfigType {
+	_: boolean;
+}
+
+export interface MetaDataType {
+	title: string;
+	icon: string;
+	channels: [string];
+	at_channel: boolean;
+	at_here: boolean;
+	numbers: [string];
+	groups: [string];
+	_: boolean;
+}
