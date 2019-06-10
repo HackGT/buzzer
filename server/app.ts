89ddf5d07ce97edc163ea37345e8d0a46d0efb96
Stephanie <almeidasmail@gmail.com>
2019-06-10 12:44:57 -0400

    Update slack plugin

--- server/app.ts
+++ server/app.ts
@@ -89,21 +89,7 @@ const resolvers = {
 			});
 
 			const sendingQueue = await Promise.all(checkQueue);
-			return await Promise.all(sendingQueue.map(f => {
-
-				f().then(result => {
-					const p = result.plugin.split(/(?=[A-Z])/).join('_').toLowerCase();
-					const insertArg = {
-						message: args.message,
-						config: args.plugins[p],
-						_id: args._id,
-						createdAt: args.createdAt,
-						errors: result.errors
-					};
-					db[upperCamel(result.plugin)].insert(insertArg);
-				}).catch(err => console.log(err));
-				return f();
-			})); // Send all!
+			return await Promise.all(sendingQueue.map(f => f())); // Send all!
 		}
 	}
 };
@@ -116,7 +102,7 @@ const schema = makeExecutableSchema({
 app.use(
 	'/graphql',
 	bodyParser.json(),
-	isAdmin,
+	//isAdmin,
 	graphqlExpress({
 		schema
 	})
