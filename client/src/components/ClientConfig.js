97f8c619b67e8b5d2d47e57f9ab6a82036cf5bce
Rahul Rajan <rahul.rajanus@gmail.com>
2019-10-20 05:18:00 -0400

    mapgt-cms dropdown

--- client/src/components/ClientConfig.js
+++ client/src/components/ClientConfig.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { Dropdown, Input, Grid, Header } from 'semantic-ui-react'
 import clientData from '../data/clients'
+import getCMS from '../data/getCMS'
 import './css/ClientConfig.css';
+
+var CMSData = {};
 class ClientConfig extends Component {
+
 	constructor() {
 		super()
+
 		this.state = {
 		};
 	};
+	componentDidMount() {
+		getCMS().then(data => {
+			CMSData = data;
+		});
+	}
 	render() {
         let config = this.props.config
         let combinedConfigUI =
@@ -18,7 +28,24 @@ class ClientConfig extends Component {
                         client.charAt(0).toUpperCase() + client.slice(1).replace("_","")}
                 </Header>
                 {Object.keys(this.props.config[client]).map(input => {
-                    switch(config[client][input]) {
+                    switch(config[client][input]["type"]) {
+						case "dropdown":
+							const dropdownValues = CMSData.data[config[client][input].cms];
+							var dropdownConfig = dropdownValues.map((data, indx) => {return {
+								text: data[config[client][input]["text"]],
+								value: data[config[client][input]["value"]],
+								key: indx + ""
+							}});
+							return (<Dropdown
+								placeholder= {"" + input}
+								className="dropdownCustom"
+								compact
+								fluid
+								selection
+								options={dropdownConfig}
+								onChange={(e,{value})=>this.props.onConfigChange(value, input, client)}
+							/>)
+
                         case "string":
                             return (
                                     <Input className="inputCustom" placeholder={input}
@@ -60,7 +87,6 @@ class ClientConfig extends Component {
             </div>
             )
         })
-        console.log(combinedConfigUI)
         return (
             <div>
                 {combinedConfigUI}
